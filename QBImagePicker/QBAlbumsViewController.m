e59000a8f9a8fb46c5cf52f366f50053dbbd1ea6
Mauro <mtet88@gmail.com>
2017-10-10 17:27:09 +0200

    Bug fix

--- QBImagePicker/QBAlbumsViewController.m
+++ QBImagePicker/QBAlbumsViewController.m
@@ -49,7 +49,10 @@ static CGSize CGSizeScale(CGSize size, CGFloat scale) {
     self.fetchResults = @[smartAlbums, userAlbums];
     
     [self updateAssetCollections];
-    
+
+	self.navigationController.navigationBar.barTintColor = self.imagePickerController.navigationBarBackgroundTintColor;
+	self.navigationController.navigationBar.tintColor = self.imagePickerController.navigationBarTintColor;
+
     // Register observer
     [[PHPhotoLibrary sharedPhotoLibrary] registerChangeObserver:self];
 }

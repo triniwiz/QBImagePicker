de7cfc96c96ccb404f3f950e6213504c3fd40558
Mauro <mtet88@gmail.com>
2021-07-26 16:30:12 +0200

    (STRATODRVE-5686) Supported iOS from 13

--- QBImagePicker/QBAssetsViewController.m
+++ QBImagePicker/QBAssetsViewController.m
@@ -697,7 +697,8 @@ static CGSize CGSizeScale(CGSize size, CGFloat scale) {
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
 {
     NSUInteger numberOfColumns;
-    if (UIInterfaceOrientationIsPortrait([[UIApplication sharedApplication] statusBarOrientation])) {
+
+    if (self.traitCollection.horizontalSizeClass == UIUserInterfaceSizeClassCompact) {
         numberOfColumns = self.imagePickerController.numberOfColumnsInPortrait;
     } else {
         numberOfColumns = self.imagePickerController.numberOfColumnsInLandscape;
